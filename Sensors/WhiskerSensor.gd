8eb09ea2bcf3d592f91ba882cbff5985be343384
Mihai Anca <mihai.anca13@gmail.com>
2021-05-15 15:31:53 +0100

    added whisker texture extraction func

--- Sensors/WhiskerSensor.gd
+++ Sensors/WhiskerSensor.gd
@@ -1,4 +1,31 @@
-extends RigidBody
+extends "BaseSensor.gd"
+
+onready var bodies = []
+onready var register = false
+
+func _ready():
+	type = "whiskers"
+
+func _process(delta):
+	pass
+	if register and bodies.size() > 0:
+		for c in bodies[0].get_children():
+			if c.get_class() == "MeshInstance":
+				print(c.get_active_material(0).get_texture(0))
 
 func _on_Area_body_entered(body):
-	print(body)
+	bodies.append(body)
+	
+	if not register and bodies.size() == 2:
+		register = true
+		bodies.clear()
+
+func _on_Area_body_exited(body):
+	if body in bodies:
+		bodies.erase(body)
+
+func render_view():
+	if bodies.size() > 0:
+		for c in bodies[0].get_children():
+			if c.get_class() == "MeshInstance":
+				return c.get_active_material(0).get_texture(0)
