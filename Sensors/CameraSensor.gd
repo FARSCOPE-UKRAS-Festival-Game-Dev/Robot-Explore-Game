5fa6982ed12b49751f2f6d3b3b0087a47c198eb7
Mickey Li <mickeyhli@outlook.com>
2021-05-15 16:47:55 +0100

    Added cave model and fixed camera positioning issue

--- Sensors/CameraSensor.gd
+++ Sensors/CameraSensor.gd
@@ -1,25 +1,22 @@
 extends "BaseSensor.gd"
 
 #Size of the viewport in pixels. Not sure how this will work on different devices 
-export var camera_resolution = Vector2(400,500)
+export var camera_resolution = Vector2(400, 500)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CameraBody/Viewport.size = camera_resolution
-
+	$Viewport.size = camera_resolution
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	pass
 	#Camera doesn't follow parent's tranform so we set it manually
-	$CameraBody/Viewport/Camera.global_transform = $CameraBody.global_transform
-	$CameraBody/Viewport/Camera.translate_object_local (Vector3(0,1,0))
-	$CameraBody/Viewport/Camera.rotate_object_local(Vector3(0, 1, 0),-PI/2) #I don't know why it's this angle 
+	$Viewport/Camera.global_transform = $gopro/CameraPosition.global_transform
 	
 func render_view():
 	#cameras will send what they capture to the nearist viewport in the tree
 	#so we make the camera a child of the viewport
-	return $CameraBody/Viewport.get_texture()
+	return $Viewport.get_texture()
