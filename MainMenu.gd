4a37ebec6e4485768fd5c071dcfaf87dbc6520ae
Mihai Anca <mihai.anca13@gmail.com>
2021-05-15 11:31:18 +0100

    Added Main Menu

---
+++ MainMenu.gd
@@ -0,0 +1,43 @@
+extends MarginContainer
+
+const scene = preload("res://Testing_Cave.tscn")
+
+onready var selector_one = $CenterContainer/VBoxContainer/VBoxContainer/CenterContainer/HBoxContainer/Selector
+onready var selector_two = $CenterContainer/VBoxContainer/VBoxContainer/CenterContainer2/HBoxContainer/Selector
+onready var selector_three = $CenterContainer/VBoxContainer/VBoxContainer/CenterContainer3/HBoxContainer/Selector
+
+var current_selection = 0
+
+func _ready():
+	set_current_selection(0)
+	
+func _process(delta):
+	if Input.is_action_just_pressed("ui_down") and current_selection < 2:
+		current_selection += 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_up") and current_selection > 0:
+		current_selection -= 1
+		set_current_selection(current_selection)
+	elif Input.is_action_just_pressed("ui_accept"):
+		handle_selection(current_selection)
+
+func handle_selection(_current_selection):
+	if _current_selection == 0:
+		get_parent().add_child(scene.instance())
+		queue_free()
+	elif _current_selection == 1:
+		print("Not yet implemented")
+	elif _current_selection == 2:
+		get_tree().quit()
+
+func set_current_selection(_current_selection):
+	selector_one.text = ""
+	selector_two.text = ""
+	selector_three.text = ""
+
+	if _current_selection == 0:
+		selector_one.text = ">"
+	elif _current_selection == 1:
+		selector_two.text = ">"
+	elif _current_selection == 2:
+		selector_three.text = ">"
