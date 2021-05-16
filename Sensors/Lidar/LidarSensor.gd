d8707a18141c7df9ee9c7428d47084e557a7ffe8
kirstyaquilina <aquilakir@hotmail.com>
2021-05-16 13:56:24 +0100

    output length of vector

--- Sensors/Lidar/LidarSensor.gd
+++ Sensors/Lidar/LidarSensor.gd
@@ -1,34 +1,45 @@
-extends "BaseSensor.gd"
-
+extends "res://Sensors/BaseSensor.gd"
+# occupancy map
 export var lidar_resolution = Vector2(400,500)
+
 var rotation_speed = 90.0 # The angular speed (degrees/s)
 var angle_range = 45.0 #The maximum deviation from forward dir that will get readings (degrees)
 var poll_rate = 3.0 #distance reads per second (Hz)
 var noise = 1.5 # standard deviation of distance read noise- (make % based?)
 var max_range = 50 # maximum range of lidar
+var hit_location
+var head_location
 
 func scanning():
 	var ray = $LidarBody/Head/RayCast
 	#print($LidarBody/Head.rotation_degrees)
 	#ray.cast_to = Vector3(randi()*1000,randi()*1000,randi()*1000)
 	ray.force_raycast_update()
-	if ray.is_colliding():
-		var hit = ray.get_collision_point()
-		print(hit)
+	var flag= ray.is_colliding()
+	#rint(flag)
+	var hit=[]
+	if flag:
+		hit=ray.get_collision_point()
+	#print([flag,hit])
+	return [flag,hit]
 
 func _ready():
 	$Viewport.size=lidar_resolution
 	$LidarPlot.flip_v = true
-	var location= $LidarBody/Head.global_transform.origin
-	print(location)
+	head_location= $LidarBody/Head.global_transform.origin
+
 	
 
 func _process(delta):
 	$LidarBody/Head.rotate_y(deg2rad(rotation_speed)*delta)
 	$LidarBody/Head/RayCast.rotate_x(deg2rad(rotation_speed)*delta)
-	scanning()
-	
+	head_location= $LidarBody/Head.global_transform.origin
+	hit_location = scanning()
+	if hit_location[0]:
+		var laserVec = hit_location[1] - head_location
+		print(hit_location[1].length())
 	#print($LidarBody/Head.rotation)
+
 
 
 func _render_view():
