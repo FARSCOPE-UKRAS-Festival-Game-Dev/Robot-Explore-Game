5b794f3739531d6e0282c8212fd72bb1e7c3e307
Mickey Li <mickeyhli@outlook.com>
2021-05-16 14:50:32 +0100

    Ported Control Panel over to new folder structure

--- Robot/Control_Panel.gd
+++ Robot/Control_Panel.gd
@@ -1,9 +1,9 @@
 extends Control
 
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var camera_hud = $MarginContainer/Panel/MarginContainer/GridContainer/Panel/MarginContainer/Camera_Sensor_HUD
+onready var lidar_hud = $MarginContainer/Panel/MarginContainer/GridContainer/Panel2/MarginContainer/Lidar_Sensor_HUD
+onready var whisker_hud = $MarginContainer/Panel/MarginContainer/GridContainer/Panel3/MarginContainer/Whisker_Sensor_HUD
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,5 +14,17 @@ func _ready():
 func _process(delta):
 	pass
 
-func set_render_target(target_texture):
-	$Panel/TextureRect.texture = target_texture 
+func set_camera_render_target(texture):
+	var image = texture.get_data()
+	var width = camera_hud.rect_size[0]
+	var height = camera_hud.rect_size[1]
+	image.resize(width, height)
+	var itex = ImageTexture.new()
+	itex.create_from_image(image)
+	camera_hud.set_render_target(texture)
+
+func set_lidar_render_target(texture):
+	lidar_hud.set_render_target(texture)
+	
+func set_whisker_render_target(texture):
+	whisker_hud.set_render_target(texture)
--- Robot/Robot_with_sensors.gd
+++ Robot/Robot_with_sensors.gd
@@ -5,4 +5,5 @@ extends Spatial
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Get the viewport texture to display it to the GUI, we only need to do this once for viewports
-	$ControlPanel.set_camera_render_target($Robot/Sensor.render_view())
+	$ControlPanel.set_camera_render_target($Robot/CameraSensor.render_view())
+	$ControlPanel.set_whisker_render_target($Robot/WhiskerSensor.render_view())
