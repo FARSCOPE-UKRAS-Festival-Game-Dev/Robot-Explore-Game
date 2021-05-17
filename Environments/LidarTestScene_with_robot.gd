6974a7747a48feb105648f9336f3850f9a4267e8
Julian <j.hird@bristol.ac.uk>
2021-05-17 13:36:47 +0100

    Added LIDAR testing enviroment with robot

--- Environments/LidarTestScene_with_robot.gd
+++ Environments/LidarTestScene_with_robot.gd
@@ -3,8 +3,8 @@ extends Spatial
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#Get the viewport texture to display it to the GUI, we only need to do this once for viewports
-
-	$ControlPanel.set_sensor_classes([$Lidar,$SideView,$TopView])
+	$Robot_with_sensor/ControlPanel.queue_free()
+	$ControlPanel.set_sensor_classes([$Robot_with_sensor/Robot/Lidar,$SideView,$TopView])
 	$ControlPanel.set_sensor_descriptions(["Lidar View","Side View","Top View"])
 	
 
