5bf6520eea4f88ed3c8e72cec41afad7fccd761b
hendrikE <50242710+hendrikE@users.noreply.github.com>
2021-05-26 00:57:18 +0100

    updated thermometer

---
+++ Sensors/TemperatureSensor.gd
@@ -0,0 +1,23 @@
+extends "BaseSensor.gd"
+
+
+# Distances
+var temperature = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# Call temp texture
+# Determine value
+# Rescale from 0 to 100
+func _process(delta):
+	pass
+
+# Render temperature taken by sensor
+func _render_view():
+	# Set thermometer value to temperature
+	pass
