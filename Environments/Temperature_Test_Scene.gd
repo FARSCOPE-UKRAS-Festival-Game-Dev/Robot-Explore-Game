b77ac70d3085dd4a5de3a00634a55b3bf4126f36
Julian <j.hird@bristol.ac.uk>
2021-05-27 21:15:38 +0100

    Integrated temperature sensor with HUD system but meant using space reserved for joystick

--- Environments/Temperature_Test_Scene.gd
+++ Environments/Temperature_Test_Scene.gd
@@ -3,20 +3,16 @@ extends Spatial
 var texture = ImageTexture.new()
 var tex_render = Image.new()
 onready var overlay = $PanelContainer/Overlay
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
+# Test scene for temperature sensor. An overhead view and position of the robot within the heatmap are displayed
 func _ready():
 	$Robot_with_sensor/Robot/TemperatureSensor.calibrate_from_body($Temp_calibration_heatmap)
 	tex_render.load("res://Assets/heatmap.png")
 	tex_render.convert(Image.FORMAT_RGB8)
 	texture.create_from_image(tex_render)
-	$Thermometer.register_sensor($Robot_with_sensor/Robot/TemperatureSensor)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+
+func _process(_delta):
 	var tex_pos = $Robot_with_sensor/Robot/TemperatureSensor.get_current_texture_pos()
 	
 	
