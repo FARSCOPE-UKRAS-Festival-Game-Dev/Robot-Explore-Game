f0549a505213b9de9d5f238934dd543eb752bb75
Skyrah1 <zhunchenyek1@gmail.com>
2021-05-30 14:32:04 +0100

    Limit when the robot aligns its transform with the floor to when the floor's incline is below an angle

--- Robot/TankDriveRobot.gd
+++ Robot/TankDriveRobot.gd
@@ -4,6 +4,7 @@ extends KinematicBody
 export var gravity = Vector3.UP * -4
 export var speed = 4
 export var rot_speed = 0.85
+export var max_floor_angle_degrees = 30
 
 export var engine_power = 3.0
 export var braking = -9.0
@@ -45,16 +46,18 @@ func _physics_process(delta):
 	
 	# move_and_slide takes the PRE-DELTA velocity, so don't multiply anything
 	# in velocity by delta
-	velocity = move_and_slide(velocity, Vector3.UP, true, 4, deg2rad(30))
+	velocity = move_and_slide(velocity, Vector3.UP, true, 4, deg2rad(max_floor_angle_degrees))
 	var n_left1 = $Body/Track1/RayCast.get_collision_normal()
 	var n_left2 = $Body/Track1/RayCast2.get_collision_normal()
 	var n_right1 = $Body/Track2/RayCast.get_collision_normal()
 	var n_right2 = $Body/Track2/RayCast2.get_collision_normal()
 	var normal = ((n_left1 + n_left2 + n_right1 + n_right2) / 4.0).normalized()
-	var angle = normal.dot(Vector3.UP)
+	var normal_dot_up = normal.dot(Vector3.UP)
+	var min_dot_limit = cos(deg2rad(max_floor_angle_degrees))
 	
-	var xform = align_with_y(global_transform, normal)
-	global_transform = global_transform.interpolate_with(xform, 0.2)
+	if normal_dot_up > min_dot_limit:
+		var xform = align_with_y(Transform(global_transform), normal)
+		global_transform = global_transform.interpolate_with(xform, 0.2)
 
 func get_input(delta):
 	var vy = velocity.y
