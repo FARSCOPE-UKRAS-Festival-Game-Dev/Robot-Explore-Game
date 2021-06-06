0b514dd41fe2d15928b0a077be7f097b01df8b48
Julian <j.hird@bristol.ac.uk>
2021-06-06 22:30:17 +0100

    Added collect sample button

--- Robot/ControlPanelElements/SpecialsMenu.gd
+++ Robot/ControlPanelElements/SpecialsMenu.gd
@@ -2,14 +2,14 @@ extends Control
 
 
 onready var background_full = $BackgroundPanel
-onready var background = $MarginContainer/VBoxContainer/AspectRatioContainer/TopOnlyBackground
-onready var button_container = $MarginContainer/VBoxContainer/AspectRatioContainer/MarginContainer/ButtonContainer
+onready var background = $AspectRatioContainer/MarginContainer/VBoxContainer/AspectRatioContainer/TopOnlyBackground
+onready var button_container = $AspectRatioContainer/MarginContainer/VBoxContainer/AspectRatioContainer/MarginContainer/ButtonContainer
 
-onready var open_button = $MarginContainer/VBoxContainer/MarginContainer/OpenSpecialsButton
-
+onready var open_button = $AspectRatioContainer/MarginContainer/VBoxContainer/MarginContainer/OpenSpecialsButton
 
 signal drill_button_pressed
 signal take_picture_button_pressed
+signal collect_sample_button_pressed
 
 func _ready():
 	set_menu_visible(false)
@@ -22,9 +22,15 @@ func _on_TakeHighResPictureButton_pressed():
 	emit_signal("take_picture_button_pressed")
 	set_menu_visible(false)
 
+func _on_TakeSampleButtom_pressed():
+	emit_signal("collect_sample_button_pressed")
+	set_menu_visible(false)
+
 func set_menu_visible(visible):
 	background.visible = visible
 	button_container.visible = visible
 	open_button.pressed = visible
 func _on_OpenSpecialsButton_pressed():
 	set_menu_visible(open_button.pressed)
+
+
