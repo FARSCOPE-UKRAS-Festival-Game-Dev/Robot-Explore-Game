1c6c0c1087c8d83115172e8295ab9d18ccdd68d3
Skyrah1 <zhunchenyek1@gmail.com>
2021-06-06 18:27:40 +0100

    Clean-up

--- MainMenu.gd
+++ MainMenu.gd
@@ -12,9 +12,9 @@ export(Array, Array, String) var scene_locations = [
 const dialog = preload("res://Utilities/Overlays/Textbox.tscn")
 
 onready var globals = get_node('/root/Globals')
-onready var selector_one = $MainMenu/MainPage/VBoxContainer/HBoxContainer/Selector
-onready var selector_two = $MainMenu/MainPage/VBoxContainer/HBoxContainer2/Selector
-onready var selector_three = $MainMenu/MainPage/VBoxContainer/HBoxContainer3/Selector
+onready var selector_one = $MainMenu/MainPage/VBoxContainer/StartHBoxContainer/Selector
+onready var selector_two = $MainMenu/MainPage/VBoxContainer/OptionsHBoxContainer/Selector
+onready var selector_three = $MainMenu/MainPage/VBoxContainer/ExitHBoxContainer/Selector
 onready var main_menu = $MainMenu/MainPage
 
 var current_selection = 0
