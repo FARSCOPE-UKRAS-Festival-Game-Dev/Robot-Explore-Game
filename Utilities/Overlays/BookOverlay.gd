2fe9fb43ebbcede7aca7a87f7ee26401ed4ed5a0
weijiewong1991 <wongweijie1991@gmail.com>
2021-06-08 22:44:16 +0100

    Fixing problems after update from master
    Fixed specials menu icons.
    Fixed book overlay to cover screen.
    Fixed exit book overlay button.

--- Utilities/Overlays/BookOverlay.gd
+++ Utilities/Overlays/BookOverlay.gd
@@ -7,7 +7,7 @@ onready var overlay_tabs = $OverlayTabs
 onready var mission_vbox = overlay_tabs.get_node("PanelContainer/MarginContainer/TabContainer/Mission/ScrollContainer/MissionVBox")
 onready var journal_vbox = overlay_tabs.get_node("PanelContainer/MarginContainer/TabContainer/Journal/ScrollContainer/JournalVBox")
 onready var options_panel = $OverlayTabs/PanelContainer/MarginContainer/TabContainer/Options/OptionsPanel
-onready var book_button = Globals.robot.get_node("ControlPanel/HUD/ButtonContainer/VBoxContainer/MarginContainer/OpenBookButton")
+onready var book_button = Globals.robot.get_node("ControlPanel/HUD/ButtonContainer/AspectRatioContainer/VBoxContainer/MarginContainer/OpenBookButton")
 
 var mission_ui = preload("res://Utilities/Overlays/BookElements/MissionItem.tscn")
 var log_entry = preload("res://Utilities/Overlays/BookElements/LogEntry.tscn")
